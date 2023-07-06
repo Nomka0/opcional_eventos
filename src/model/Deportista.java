2bfa861e1d9e3f0b7909499482c8cef9dceb86e3
nomka0 <juanjose111102@gmail.com>
2023-07-06 14:48:16 -0500

    dao y modelo (parcialmente)

--- src/model/Deportista.java
+++ src/model/Deportista.java
@@ -18,37 +18,11 @@ public class Deportista {
 	public Deportista() {
 
 	}
-	
-
-	public String getNombre() {
-		return nombre;
-	}
-	
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public long getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(long telefono) {
-		this.telefono = telefono;
-	}
-
-	public String getDireccion() {
-		return direccion;
-	}
-
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
 
 	public int getId() {
 		return id;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
@@ -56,10 +30,27 @@ public class Deportista {
 	public String getApellido() {
 		return apellido;
 	}
-	
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDeporte() {
+		return deporte;
+	}
+
+	public void setDeporte(String deporte) {
+		this.deporte = deporte;
+	}
+	
 	
 }
 
